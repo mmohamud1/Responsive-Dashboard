1fea1a81133806f617352acd6611ff857d455645
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-30 13:44:20 +0000

    style hamburger menu

--- script.js
+++ script.js
@@ -60,3 +60,21 @@ var chart = new ApexCharts(document.querySelector('#apex1'), options);
 chart.render();
 
 // Sidebar Ressponsive
+
+var sidebarOpen = false;
+var sidebar = document.getElementById('sidebar');
+var sidebarCloseIcon = document.getElementById('sidebarIcon');
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar_responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar_responsive');
+    sidebarOpen = false;
+  }
+}
